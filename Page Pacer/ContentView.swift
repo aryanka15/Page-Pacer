c35d351104f11485b2a944040e2e75e3bcee988c
aryanka15 <92349329+aryanka15@users.noreply.github.com>
2022-11-05 14:39:51 -0700

    Initial Commit

---
+++ Page Pacer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Page Pacer
+//
+//  Created by Aryan Karani on 11/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Page Pacer/Page_PacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Page_PacerApp.swift
+//  Page Pacer
+//
+//  Created by Aryan Karani on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Page_PacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
